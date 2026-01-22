6fcdafcb02f09dc8904512908ac63eb10cd9697f
Oluwadara Osunkoya <dara@dhcp-10-132-207-229.uawifi.arizona.edu>
2026-01-22 09:29:25 -0700

    Change App.java to Gradenator.java

--- src/com/gradescope/gradenator/Gradenator.java
+++ src/com/gradescope/gradenator/Gradenator.java
@@ -1,9 +1,8 @@
-
 package com.gradescope.gradenator;
 import java.io.File;
 import java.util.Scanner;
 
-public class App {
+public class Gradenator {
 
     public static void main(String[] args) throws Exception {
         // Variables
